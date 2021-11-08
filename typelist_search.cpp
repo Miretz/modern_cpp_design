6e7259c03625d6f784f5f1b0fa2b12c1e89b5b6e
miretz <1217795+Miretz@users.noreply.github.com>
2021-11-09 00:27:09 +0100

    searching typelist

---
+++ typelist_search.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <type_traits>
+
+#include "loki-0.1.7/include/loki/Typelist.h"
+
+using Loki::Typelist;
+using namespace Loki::TL;
+
+class NullType {};
+
+template <class TList, class T> struct MyIndexOf;
+
+template <class T> struct MyIndexOf<NullType, T> {
+  enum { value = -1 };
+};
+
+template <class T, class Tail> struct MyIndexOf<Typelist<T, Tail>, T> {
+  enum { value = 0 };
+};
+
+template <class Head, class Tail, class T>
+struct MyIndexOf<Typelist<Head, Tail>, T> {
+private:
+  enum { temp = MyIndexOf<Tail, T>::value };
+
+public:
+  enum { value = temp == -1 ? -1 : 1 + temp };
+};
+
+auto main() -> int {
+  typedef Typelist<
+      signed char,
+      Typelist<short int, Typelist<int, Typelist<long int, NullType>>>>
+      SignedIntegrals;
+
+  int search_result = MyIndexOf<SignedIntegrals, int>::value;
+
+  std::cout << search_result << '\n';
+
+  return 0;
+}
