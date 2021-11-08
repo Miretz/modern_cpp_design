e774e2b88e0302472c0ea539dc290345ce646cb8
miretz <1217795+Miretz@users.noreply.github.com>
2021-11-08 19:46:52 +0100

    typelists indexed access

---
+++ typelist_indexed_access.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <type_traits>
+
+#include "loki-0.1.7/include/loki/Typelist.h"
+
+using Loki::Typelist;
+using namespace Loki::TL;
+
+class NullType {};
+
+template <class TList, unsigned int index> struct MyTypeAt;
+
+template <class Head, class Tail> struct MyTypeAt<Typelist<Head, Tail>, 0> {
+  typedef Head Result;
+};
+template <class Head, class Tail, unsigned int i>
+struct MyTypeAt<Typelist<Head, Tail>, i> {
+  typedef typename MyTypeAt<Tail, i - 1>::Result Result;
+};
+
+auto main() -> int {
+  typedef Typelist<
+      signed char,
+      Typelist<short int, Typelist<int, Typelist<long int, NullType>>>>
+      SignedIntegrals;
+
+  MyTypeAt<SignedIntegrals, 1>::Result s = 122; // short int
+  MyTypeAt<SignedIntegrals, 3>::Result l = 122; // long int
+
+  std::cout << typeid(s).name() << '\n' << typeid(l).name() << '\n';
+
+  return 0;
+}
