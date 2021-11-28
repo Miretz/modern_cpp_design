ee7921c1249faad7694e5f0cc8125462c593e7d8
miretz <1217795+Miretz@users.noreply.github.com>
2021-11-28 01:24:14 +0100

    visitor that modifies object

--- 10_visitor/doc_element_visitor.cpp
+++ 10_visitor/doc_element_visitor.cpp
@@ -18,10 +18,14 @@ public:
 };
 
 class Paragraph : public DocElement {
+  int fontSize_ = 10;
+
 public:
   void Accept(DocElementVisitor &v) override { v.VisitParagraph(*this); }
   int NumChars() { return 42; }
   int NumWords() { return 61; }
+  int GetFontSize() const { return fontSize_; }
+  void SetFontSize(int value) { fontSize_ = value; }
 };
 
 class RasterBitmap : public DocElement {
@@ -48,13 +52,24 @@ private:
   int images_ = 0;
 };
 
+// Use the visitor to add new functionalities
+class IncrementFontSize : public DocElementVisitor {
+public:
+  virtual void VisitParagraph(Paragraph &par) override {
+    par.SetFontSize(par.GetFontSize() + 1);
+  }
+  virtual void VisitRasterBitmap(RasterBitmap &) override {
+    // NOP
+  }
+};
+
 auto main() -> int {
 
   DocStats stats;
 
-  auto b1 = std::shared_ptr<DocElement>(new RasterBitmap);
-  auto b2 = std::shared_ptr<DocElement>(new RasterBitmap);
-  auto p = std::shared_ptr<DocElement>(new Paragraph);
+  auto b1 = std::shared_ptr<RasterBitmap>(new RasterBitmap);
+  auto b2 = std::shared_ptr<RasterBitmap>(new RasterBitmap);
+  auto p = std::shared_ptr<Paragraph>(new Paragraph);
 
   std::vector<std::shared_ptr<DocElement>> elements{b1, b2, p};
 
@@ -63,6 +78,10 @@ auto main() -> int {
   }
 
   stats.Display();
+
+  IncrementFontSize incrementFontSize;
+  p->Accept(incrementFontSize);
+  std::cout << "Visitor incremented font size to: " << p->GetFontSize() << '\n';
 
   return 0;
 }
