283eb2ab1db35f76d63ecb1d40a10824ca1a8af5
miretz <1217795+Miretz@users.noreply.github.com>
2021-11-29 13:23:00 +0100

    generic cyclic visitor

---
+++ 10_visitor/generic_cyclic_visitor.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+
+#include "../loki-0.1.7/include/loki/HierarchyGenerators.h"
+#include "../loki-0.1.7/include/loki/Typelist.h"
+#include "../loki-0.1.7/include/loki/TypelistMacros.h"
+#include "../loki-0.1.7/include/loki/Visitor.h"
+
+using Loki::Typelist;
+using Loki::Visitor;
+
+// Generic Cyclic Visitor - Cyclic is faster than Acyclic because there is
+// no need for dynamic cast
+
+class DocElement;
+class Paragraph;
+class RasterBitmap;
+
+template <typename R, class TList>
+class CyclicVisitor : public Visitor<TList, R> {
+public:
+  using ReturnType = R;
+  template <class Visited> ReturnType Visit(Visited &host) {
+    Visitor<Visited, ReturnType> &subObj = *this;
+    return subObj.Visit(host);
+  }
+};
+
+typedef CyclicVisitor<void,
+                      LOKI_TYPELIST_3(DocElement, Paragraph, RasterBitmap)>
+    MyVisitor;
+
+// clang-format off
+#define DEFINE_CYCLIC_VISITABLE(SomeVisitor) \
+    virtual SomeVisitor::ReturnType Accept(SomeVisitor& guest) \
+    { return guest.Visit(*this); }
+// clang-format on
+
+// Usage example
+
+class DocElement {
+public:
+  DEFINE_CYCLIC_VISITABLE(MyVisitor);
+  virtual void Visit(MyVisitor &) = 0;
+};
+
+class Paragraph : public DocElement {
+public:
+  DEFINE_CYCLIC_VISITABLE(MyVisitor);
+  void Visit(MyVisitor&) override { std::cout << "Hello, from Paragraph.\n"; }
+};
+
+class RasterBitmap : public DocElement {
+public:
+  DEFINE_CYCLIC_VISITABLE(MyVisitor);
+  void Visit(MyVisitor&) override { std::cout << "Hello, from RasterBitmap.\n"; }
+};
+
+class MyConcreteVisitor : public MyVisitor {
+public:
+  void Visit(DocElement &) override { std::cout << "Visit(DocElement&) \n"; }
+  void Visit(Paragraph &) override { std::cout << "Visit(Paragraph&) \n"; }
+  void Visit(RasterBitmap &) override {
+    std::cout << "Visit(RasterBitmap&) \n";
+  }
+};
+
+auto main() -> int {
+  MyConcreteVisitor visitor;
+  Paragraph par;
+  DocElement *d = &par;
+  d->Accept(visitor);
+  return 0;
+}
