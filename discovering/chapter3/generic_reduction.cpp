e21721e46f3031d716a3635e7d610876cb14007c
miretz <1217795+Miretz@users.noreply.github.com>
2021-12-05 17:24:09 +0100

    generic reduction

---
+++ discovering/chapter3/generic_reduction.cpp
@@ -0,0 +1,38 @@
+#include <functional>
+#include <iostream>
+#include <numeric>
+#include <vector>
+
+template <typename Iter, typename T, typename BinaryFunction>
+T my_accumulate(Iter it, Iter end, T init, BinaryFunction op) {
+    for (; it != end; ++it) {
+        init = op(init, *it);
+    }
+    return init;
+}
+
+template <typename T> struct add {
+    T operator()(const T &x, const T &y) const { return x + y; }
+};
+
+// we could also parametrize the oparator()
+struct times {
+    template <typename T> T operator()(const T &x, const T &y) const {
+        return x * y;
+    }
+};
+
+auto main() -> int {
+    std::vector<double> v = {7.0, 8.0, 11.0};
+    double s = my_accumulate(v.begin(), v.end(), 0.0, add<double>{});
+    double p = my_accumulate(v.begin(), v.end(), 1.0, times{});
+
+    std::cout << "Sum: " << s << '\n';
+    std::cout << "Product: " << p << '\n';
+
+    // std also has this - needs <numeric> and <functional> headers
+    double s2 = std::accumulate(v.begin(), v.end(), 0.0, std::plus<double>{});
+    std::cout << "STL Sum: " << s2 << '\n';
+
+    return 0;
+}
