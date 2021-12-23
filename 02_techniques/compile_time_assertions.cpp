421fff9d46ffd7eb4df9f06c21f84eb6913c4cb5
miretz <1217795+Miretz@users.noreply.github.com>
2021-12-23 01:05:30 +0100

    reformatting old files

--- 02_techniques/compile_time_assertions.cpp
+++ 02_techniques/compile_time_assertions.cpp
@@ -13,21 +13,21 @@ template <> struct CompileTimeChecker<false> {};
 // clang-format on
 
 template <class To, class From> To safe_reinterpret_cast(From from) {
-  STATIC_CHECK(sizeof(From) <= sizeof(To), Destination_Type_Too_Narrow);
-  return reinterpret_cast<To>(from);
+    STATIC_CHECK(sizeof(From) <= sizeof(To), Destination_Type_Too_Narrow);
+    return reinterpret_cast<To>(from);
 }
 
 auto main() -> int {
 
-  // no error
-  char st[] = "some text\0";
-  void *c = safe_reinterpret_cast<void *>(st);
-  char *cs = safe_reinterpret_cast<char *>(c);
-  std::cout << cs << '\n';
+    // no error
+    char st[] = "some text\0";
+    void *c = safe_reinterpret_cast<void *>(st);
+    char *cs = safe_reinterpret_cast<char *>(c);
+    std::cout << cs << '\n';
 
-  // uncomment these to see the compile time error
-  // char fake = safe_reinterpret_cast<char>(cs);
-  // std::cout << fake << '\n';
+    // uncomment these to see the compile time error
+    // char fake = safe_reinterpret_cast<char>(cs);
+    // std::cout << fake << '\n';
 
-  return 0;
+    return 0;
 }
