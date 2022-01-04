a23b3c74606daf558b4c3d91c9432a3721f729cd
miretz <1217795+Miretz@users.noreply.github.com>
2022-01-04 09:42:48 +0100

    added c++14 version

--- discovering/chapter5/common_type.cpp
+++ discovering/chapter5/common_type.cpp
@@ -12,10 +12,23 @@ minimum(const T& t, const P& ...p)
   return std::min(res_type(t), res_type(minimum(p...)));
 }
 
+// In C++14 the variadic overload of minimum simplifies the definition
+// using auto keyword
+
+template <typename T, typename ...P>
+inline auto minimum_new(const T& t, const P& ...p)
+{
+  using res_type = std::common_type_t<T, P...>;
+  return std::min(res_type(t), res_type(minimum(p...)));
+}
+
 int main()
 {
   auto s = minimum(-7, 3.6f, 9u, -42.6);
   std::cout << "s is " << s << " and its type is " << typeid(s).name() << '\n';
 
+  auto s2 = minimum_new(-7, 3.6f, 9u, -42.6);
+  std::cout << "s2 is " << s2 << " and its type is " << typeid(s2).name() << '\n';
+  
   return 0;
 }
