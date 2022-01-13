c8c6d31e94e3fc61fefe7ebda390d8e5efe36217
miretz <1217795+Miretz@users.noreply.github.com>
2022-01-13 21:51:59 +0100

    modernize windows example

--- windows/hello.cpp
+++ windows/hello.cpp
@@ -2,10 +2,9 @@
 
 // on MinGW: g++ -mwindows hello.cpp
 
-auto main() -> int
-{
-  const auto& title = "Hello, World!";
-  const auto& text  = "Click the OK button to quit.";
-  MessageBox(0, text, title, MB_SETFOREGROUND);
-  return 0;
+auto main() -> int {
+    const auto title = "Hello, World!";
+    const auto text = "Click the OK button to quit.";
+    MessageBox(nullptr, text, title, MB_SETFOREGROUND);
+    return 0;
 }
