c04f500f80f391d1bc0f734aa9c125b19b58cfb4
miretz <1217795+Miretz@users.noreply.github.com>
2022-01-14 20:16:17 +0100

    modernize local classes

--- 02_techniques/local_classes.cpp
+++ 02_techniques/local_classes.cpp
@@ -1,39 +1,46 @@
 #include <iostream>
 #include <memory>
-#include <string_view>
+#include <string>
+#include <utility>
 
 class Interface {
-public:
-  virtual void fun() = 0;
-  virtual ~Interface() = default;
+  public:
+    virtual void fun() = 0;
+    Interface() = default;
+    virtual ~Interface() = default;
+
+    Interface(Interface &) = delete;
+    Interface(Interface &&) = delete;
+    auto operator=(Interface &) -> Interface & = delete;
+    auto operator=(Interface &&) -> Interface & = delete;
 };
 
 template <class T, class P>
-std::unique_ptr<Interface> make_adapter(const T &obj, const P &arg) {
-  class Local : public Interface {
-  public:
-    Local(const T &obj, const P &arg) : obj_(obj), arg_(arg) {}
-    virtual void fun() { obj_.call(arg_); }
+auto make_adapter(const T &obj, const P &arg) -> std::unique_ptr<Interface> {
+    class Local : public Interface {
+      public:
+        Local(const T &obj, P arg) : obj_(obj), arg_(std::move(arg)) {}
+        void fun() override { obj_.call(arg_); }
 
-  private:
-    T obj_;
-    P arg_;
-  };
-  return std::make_unique<Local>(obj, arg);
+      private:
+        T obj_;
+        P arg_;
+    };
+    return std::make_unique<Local>(obj, arg);
 };
 
 class Test {
-public:
-  void call(std::string_view i) { std::cout << i; }
+  public:
+    void call(const std::string &i) { std::cout << i; }
 };
 
 auto main() -> int {
 
-  constexpr Test original;
-  constexpr std::string_view text = "Foo\n";
+    constexpr Test original;
+    constexpr auto text = "Foo\n";
 
-  auto i = make_adapter<Test, std::string_view>(original, text);
-  i->fun();
+    auto i = make_adapter<Test, std::string>(original, text);
+    i->fun();
 
-  return 0;
+    return 0;
 }
