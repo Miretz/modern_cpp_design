2dad3a51d1bfa46d0064f8813f5b2e1cad19443a
miretz <1217795+Miretz@users.noreply.github.com>
2022-01-15 22:54:07 +0100

    modernize pointer traits

--- 02_techniques/pointer_traits.cpp
+++ 02_techniques/pointer_traits.cpp
@@ -4,26 +4,25 @@
 class NullType {};
 
 template <typename T> class TypeTraits {
-private:
-  // pointer detection
-  template <class U> struct PointerTraits {
-    enum { result = false };
-    typedef NullType PointeeType;
-  };
-  template <class U> struct PointerTraits<U *> {
-    enum { result = true };
-    typedef U PointeeType;
-  };
+  private:
+    // pointer detection
+    template <class U> struct PointerTraits {
+        enum { result = false };
+        using PointeeType = NullType;
+    };
+    template <class U> struct PointerTraits<U *> {
+        enum { result = true };
+        using PointeeType = U;
+    };
 
-public:
-  enum { isPointer = PointerTraits<T>::result };
-  typedef typename PointerTraits<T>::PointeeType pType;
+  public:
+    enum { isPointer = PointerTraits<T>::result };
+    using pType = typename PointerTraits<T>::PointeeType;
 };
 
 auto main() -> int {
-
-  const bool iterIsPtr = TypeTraits<std::vector<int>::iterator>::isPointer;
-  std::cout << "vector<int>::iterator is " << (iterIsPtr ? "fast" : "smart");
-  std::cout << '\n';
-  return 0;
+    const bool iterIsPtr = TypeTraits<std::vector<int>::iterator>::isPointer;
+    std::cout << "vector<int>::iterator is " << (iterIsPtr ? "fast" : "smart");
+    std::cout << '\n';
+    return 0;
 }
