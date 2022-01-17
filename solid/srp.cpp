abbed59d3f10ca42ed0a65fd12bb8ee2da83a31e
miretz <1217795+Miretz@users.noreply.github.com>
2022-01-17 23:41:58 +0100

    refactor srp and unoptimized

--- solid/srp.cpp
+++ solid/srp.cpp
@@ -9,132 +9,71 @@
 #include <string_view>
 #include <vector>
 
+#include "item.hpp"
+#include "new_order.hpp"
+
 // Single responsibility principle - splitting Order and Payment into separate
 // classes
-
-struct Item {
-  Item(std::string_view item, int quantity, float price)
-      : item_{item}, quantity_{quantity}, price_{price} {}
-  std::string_view item_;
-  int quantity_;
-  float price_;
-};
-
-/*
- * Order
- */
-
-enum class Status { Open, Paid };
-
-static uint32_t last_order_id = 0;
-
-class NewOrder {
-public:
-  void AddItem(const Item &new_item);
-  [[nodiscard]] float TotalPrice() const;
-  void SetStatus(Status status);
-  void PrintOrder() const;
-  [[nodiscard]] const uint32_t &GetId() const;
-
-private:
-  uint32_t id{last_order_id++}; // should be proper UUID generator - this is
-                                // just an example
-  std::vector<Item> items_;
-  Status status_{Status::Open};
-};
-
-void NewOrder::AddItem(const Item &new_item) { items_.push_back(new_item); }
-
-void NewOrder::PrintOrder() const {
-  std::cout << "The id of the order is " << id << ", with items:\n";
-  static constexpr auto column_width = 20;
-  std::cout << std::left << std::setw(column_width) << "Item" << std::left
-            << std::setw(column_width) << "Quantity" << std::left
-            << std::setw(column_width) << "Price" << std::left
-            << std::setw(column_width) << "Total" << '\n';
-  std::for_each(items_.begin(), items_.end(), [&](const auto &item) {
-    std::cout << std::left << std::setw(column_width) << item.item_ << std::left
-              << std::setw(column_width) << item.quantity_ << std::left
-              << std::setw(column_width) << item.price_ << std::left
-              << std::setw(column_width) << (item.quantity_ * item.price_)
-              << '\n';
-  });
-  std::cout << "The status is "
-            << ((status_ == Status::Paid) ? "Paid" : "Open");
-  std::cout << '\n';
-}
-
-float NewOrder::TotalPrice() const {
-  float total{0.0f};
-  for (auto &item : items_) {
-    total += static_cast<float>(item.quantity_) * item.price_;
-  }
-  return total;
-}
-
-void NewOrder::SetStatus(Status status) { status_ = status; }
-
-const uint32_t &NewOrder::GetId() const { return id; }
 
 /*
  * PAYMENT PROCESSOR
  */
 
 struct PaymentProcessor {
-  explicit PaymentProcessor(NewOrder &new_order) : new_order_{new_order} {}
+    explicit PaymentProcessor(NewOrder &new_order) : new_order_{new_order} {}
 
-  void DisplayInfo() const;
-  void PayDebit(std::string_view security_code) const;
-  void PayCredit(std::string_view security_code) const;
-  void PayPaypal(std::string_view security_code) const;
+    void DisplayInfo() const;
+    void PayDebit(const std::string &security_code) const;
+    void PayCredit(const std::string &security_code) const;
+    void PayPaypal(const std::string &security_code) const;
 
-private:
-  NewOrder &new_order_;
+  private:
+    NewOrder &new_order_;
 };
 
-void PaymentProcessor::PayDebit(std::string_view security_code) const {
-  std::cout << "Processing debit payment type\n";
-  std::cout << "Verifying security code: " << security_code << '\n';
-  new_order_.SetStatus(Status::Paid);
+void PaymentProcessor::PayDebit(const std::string &security_code) const {
+    std::cout << "Processing debit payment type\n";
+    std::cout << "Verifying security code: " << security_code << '\n';
+    new_order_.SetStatus(Status::Paid);
 }
-void PaymentProcessor::PayCredit(std::string_view security_code) const {
-  std::cout << "Processing credit payment type\n";
-  std::cout << "Verifying security code: " << security_code << '\n';
-  new_order_.SetStatus(Status::Paid);
+void PaymentProcessor::PayCredit(const std::string &security_code) const {
+    std::cout << "Processing credit payment type\n";
+    std::cout << "Verifying security code: " << security_code << '\n';
+    new_order_.SetStatus(Status::Paid);
 }
-void PaymentProcessor::PayPaypal(std::string_view security_code) const {
-  std::cout << "Processing paypal payment type\n";
-  std::cout << "Verifying security code: " << security_code << '\n';
-  new_order_.SetStatus(Status::Paid);
+void PaymentProcessor::PayPaypal(const std::string &security_code) const {
+    std::cout << "Processing paypal payment type\n";
+    std::cout << "Verifying security code: " << security_code << '\n';
+    new_order_.SetStatus(Status::Paid);
 }
 void PaymentProcessor::DisplayInfo() const {
-  std::cout << "Payment processor for order: "
-            << std::to_string(new_order_.GetId()) << '\n';
+    std::cout << "Payment processor for order: "
+              << std::to_string(new_order_.GetId()) << '\n';
 }
 
-int main() {
-  Item item1{"Keyboard", 1, 50.0};
-  Item item2{"SSD", 1, 150.0};
-  Item item3{"USB cable", 2, 5.0};
-  NewOrder an_order{};
+auto main() -> int {
+    Item item1{"Keyboard", 1, 50.0};
+    Item item2{"SSD", 1, 150.0};
+    Item item3{"USB cable", 2, 5.0};
+    NewOrder an_order{};
 
-  an_order.AddItem(item1);
-  an_order.AddItem(item2);
-  an_order.AddItem(item3);
+    an_order.AddItem(item1);
+    an_order.AddItem(item2);
+    an_order.AddItem(item3);
 
-  an_order.PrintOrder();
+    an_order.PrintOrder();
 
-  PaymentProcessor proc1{an_order};
-  proc1.DisplayInfo();
-  proc1.PayCredit("65379");
+    PaymentProcessor proc1{an_order};
+    proc1.DisplayInfo();
+    proc1.PayCredit("65379");
 
-  PaymentProcessor proc2{an_order};
-  proc2.DisplayInfo();
-  proc2.PayDebit("996366");
+    PaymentProcessor proc2{an_order};
+    proc2.DisplayInfo();
+    proc2.PayDebit("996366");
 
-  PaymentProcessor proc3{an_order};
-  proc3.DisplayInfo();
-  proc3.PayDebit("123123");
+    PaymentProcessor proc3{an_order};
+    proc3.DisplayInfo();
+    proc3.PayDebit("123123");
 
-  return 0;
+    return 0;
 }
--- solid/unoptimized.cpp
+++ solid/unoptimized.cpp
@@ -7,76 +7,78 @@
 #include <stdexcept>
 #include <string>
 #include <string_view>
+#include <utility>
 #include <vector>
 
 // Too many code smells
 
 struct Item {
-  Item(std::string_view item, int quantity, float price)
-      : item_{item}, quantity_{quantity}, price_{price} {}
-  std::string_view item_;
-  int quantity_;
-  float price_;
+    Item(std::string item, int quantity, float price)
+        : item_{std::move(item)}, quantity_{quantity}, price_{price} {}
+    std::string item_;
+    int quantity_;
+    float price_;
 };
 
 class Order {
-public:
-  void AddItem(const Item &new_item) { items_.push_back(new_item); };
-  [[nodiscard]] float TotalPrice() const {
-    return std::accumulate(items_.begin(), items_.end(), 0.0f,
-                           [](float sum, const auto &item) {
-                             return sum + (item.price_ * item.quantity_);
-                           });
-  };
-  void Pay(std::string_view payment_type, std::string_view security_code) {
-    std::cout << "Pay: " << payment_type << '\t' << security_code << '\n';
-  }
+  public:
+    void AddItem(const Item &new_item) { items_.push_back(new_item); };
+    [[nodiscard]] auto TotalPrice() const -> float {
+        return std::accumulate(items_.begin(), items_.end(), 0.0f,
+                               [](float sum, const auto &item) {
+                                   return sum + (item.price_ * item.quantity_);
+                               });
+    };
+    void Pay(const std::string &payment_type,
+             const std::string &security_code) {
+        std::cout << "Pay: " << payment_type << '\t' << security_code << '\n';
+    }
 
-  void PrintOrder() const {
-    static constexpr auto column_width = 20;
-    std::cout << std::left << std::setw(column_width) << "Item" << std::left
-              << std::setw(column_width) << "Quantity" << std::left
-              << std::setw(column_width) << "Price" << std::left
-              << std::setw(column_width) << "Total" << '\n';
-    std::for_each(items_.begin(), items_.end(), [&](const auto &item) {
-      std::cout << std::left << std::setw(column_width) << item.item_
-                << std::left << std::setw(column_width) << item.quantity_
-                << std::left << std::setw(column_width) << item.price_
-                << std::left << std::setw(column_width)
-                << (item.quantity_ * item.price_) << '\n';
-    });
-    std::cout << '\n';
-  };
+    void PrintOrder() const {
+        static constexpr auto column_width = 20;
+        std::cout << std::left << std::setw(column_width) << "Item" << std::left
+                  << std::setw(column_width) << "Quantity" << std::left
+                  << std::setw(column_width) << "Price" << std::left
+                  << std::setw(column_width) << "Total" << '\n';
+        std::for_each(items_.begin(), items_.end(), [&](const auto &item) {
+            std::cout << std::left << std::setw(column_width) << item.item_
+                      << std::left << std::setw(column_width) << item.quantity_
+                      << std::left << std::setw(column_width) << item.price_
+                      << std::left << std::setw(column_width)
+                      << (item.quantity_ * item.price_) << '\n';
+        });
+        std::cout << '\n';
+    };
 
-private:
-  std::vector<Item> items_;
-  std::string status_{"open"};
+  private:
+    std::vector<Item> items_;
+    std::string status_{"open"};
 };
 
-int main() {
-  Item item1{"Keyboard", 1, 50.0};
-  Item item2{"SSD", 1, 150.0};
-  Item item3{"USB cable", 2, 5.0};
-  Order an_order{};
+auto main() -> int {
+    Item item1{"Keyboard", 1, 50.0};
+    Item item2{"SSD", 1, 150.0};
+    Item item3{"USB cable", 2, 5.0};
+    Order an_order{};
 
-  an_order.AddItem(item1);
-  an_order.AddItem(item2);
-  an_order.AddItem(item3);
+    an_order.AddItem(item1);
+    an_order.AddItem(item2);
+    an_order.AddItem(item3);
 
-  an_order.PrintOrder();
+    an_order.PrintOrder();
 
-  std::cout << "The total price is: " << an_order.TotalPrice() << "\n\n";
-  try {
-    an_order.Pay("debit", "09878");
-  } catch (const std::exception &t) {
-    std::cout << t.what() << '\n';
-  }
+    std::cout << "The total price is: " << an_order.TotalPrice() << "\n\n";
+    try {
+        an_order.Pay("debit", "09878");
+    } catch (const std::exception &t) {
+        std::cout << t.what() << '\n';
+    }
 
-  try {
-    an_order.Pay("credit", "96553");
-  } catch (const std::exception &t) {
-    std::cout << t.what() << '\n';
-  }
+    try {
+        an_order.Pay("credit", "96553");
+    } catch (const std::exception &t) {
+        std::cout << t.what() << '\n';
+    }
 
-  return 0;
+    return 0;
 }
